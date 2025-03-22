58cb7a00ad46261324801dae2368f560ef8c3372
DarkSus666 <darksus666@gmail.com>
2025-03-22 03:35:40 +0300

    Домашка 4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,74 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int clientOS = 0;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
+        clientOS = 1;
+        int clientDeviceYear = 2007;
+        if (clientOS == 0) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        } else if (clientOS == 1) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        }
+
+        int year = 2021;
+        if (year > 1584 && year % 4 == 0) {
+            if (year % 400 != 0 && year % 100 == 0) {
+                System.out.println(year + " год не является високосным");
+            } else {
+                System.out.println(year + " год является високосным");
+            }
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+
+        int deliveryDistance = 95;
+        if (deliveryDistance <= 20) {
+            System.out.println("Потребуется дней: 1");
+        } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+            System.out.println("Потребуется дней: 2");
+        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            System.out.println("Потребуется дней: 3");
+        } else {
+            System.out.println("Доставки нет");
+        }
+
+        int monthNumber = 12;
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println(monthNumber + "-й месяц принадлежит к сезону зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println(monthNumber + "-й месяц принадлежит к сезону весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println(monthNumber + "-й месяц принадлежит к сезону лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println(monthNumber + "-й месяц принадлежит к сезону осень");
+                break;
+            default:
+                System.out.println("Такого месяца не существует");
+        }
     }
 }
